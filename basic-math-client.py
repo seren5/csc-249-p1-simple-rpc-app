8525b3574eb09d5d73f5543c1fcf12915bc9be1c
seren5 <shuyueeren@gmail.com>
2024-09-20 22:55:06 -0400

    comments on client done

--- basic-math-client.py
+++ basic-math-client.py
@@ -19,29 +19,29 @@ def talk_to_server(sock):
     
     exit_loop = False
     while exit_loop == False:
-        msg = input("What (addition/subtraction) equation would you like solved? (Please enter your equation in the following format: +1:3:5 or -1:3:5. The order of the numbers matter.)   ")
-        if msg == "quit":
+        msg = input("What (addition/subtraction) equation would you like solved? (Please enter your equation in the following format: +1:3:5 or -1:3:5. The order of the numbers matter.)   ") # Asks for user input
+        if msg == "quit": # If the user inputs quit, the program exits
             print("client quitting at operator request")
             exit(0)
-        elif msg == "":
+        elif msg == "": # If the user inputs an empty message, they are prompted to put in numbers again
             print("You have not specified a value, please try again.")
         elif msg.startswith("+") or msg.startswith("-"):
-            if ":" in msg and "0" in msg or "1" in msg or "2" in msg or "3" in msg or "4" in msg or "5" in msg or "6" in msg or "7" in msg or "8" in msg or "9" in msg:
+            if ":" in msg and "0" in msg or "1" in msg or "2" in msg or "3" in msg or "4" in msg or "5" in msg or "6" in msg or "7" in msg or "8" in msg or "9" in msg: # Checks for ":" or number (it isn't the prettiest but I couldn't get it to work in time)
                 break
-            else:
+            else: # The user has input a letter or did not start with "+" or "-"
                 print("Please format your equation in the following format:+1:3:5 or -1:3:5")
-        else:
+        else: # The user has input a letter or did not start with "+" or "-"
             print("You have not formatted your equation properly.")
 
 
 
-    sock.sendall(msg.encode('utf-8'))
+    sock.sendall(msg.encode('utf-8')) # Sends message to the server (or over the network)
     print("message sent, waiting for reply")
     
-    answer = sock.recv(1024)
+    answer = sock.recv(1024) # Receives answer from the server
     
     if not answer:
-        return False
+        return False # Server did not receive
     else:
         answer = answer.decode('utf-8') # to remove the 'b' in front of the print
         print(f"Received response: '{answer}' from server -> [{len(answer)} bytes]")
